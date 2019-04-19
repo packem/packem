1d5ea9050a2e928945f9ba90caf8327df7a65582
bukharim96 <bukharim96@gmail.com>
2019-04-19 18:08:25 +0200

    Build script fix

--- scripts/build.js
+++ scripts/build.js
@@ -30,39 +30,28 @@ const https = require("https");
 const url = require("url");
 const { sep: pathSeperator } = require("path");
 const { getAbi } = require("node-abi");
-const fetch = require("node-fetch");
+const Octokit = require("@octokit/rest");
 const { x: extractTarGz } = require("tar");
 
-const PREBUILT_REPO_DATA_URL = process.env.PREBUILT_REPO_DATA_URL || "https://api.github.com/repos/packem/packem/releases/latest";
-const PREBUILT_REPO_URL = process.env.PREBUILT_REPO_URL || "https://github.com/packem/packem/";
-
-if (!PREBUILT_REPO_DATA_URL || !PREBUILT_REPO_URL) {
-  console.error("[PACKEM ERR]: You are not authorized to build from source.");
-  process.exit(0);
-}
-
+const AUTH_TOKEN = process.env.NODE_PRE_GYP_GITHUB_TOKEN;
+const PREBUILT_REPO_OWNER = process.env.PREBUILT_REPO_OWNER || "packem";
+const PREBUILT_REPO_NAME = process.env.PREBUILT_REPO_NAME || "packem";
+const PREBUILT_REPO_URL = `https://github.com/${PREBUILT_REPO_OWNER}/${PREBUILT_REPO_NAME}`;
 const CWD = process.cwd();
 const NODE_ABI = getAbi(process.version.replace(/^v/, ""), "node");
 const PLATFORM = process.platform;
 const ARCH = process.arch;
 
-// extract then update downloaded src
-function updateBinary(response) {
-  response.pipe(
-    extractTarGz({
-      cwd: CWD,
-      filter: (path, stat) => (path === "bin/index.node" ? true : false)
-    })
-  );
-}
+const octokit = new Octokit({ auth: AUTH_TOKEN });
 
-// download prebuilt binaries
-fetch(PREBUILT_REPO_DATA_URL, {
-  headers: { "User-Agent": "https://api.github.com/meta" }
-})
-  .then(d => d.json())
-  .then(({ tag_name: VERSION }) => {
-    const TARBALL_URL = `${PREBUILT_REPO_URL}releases/download/${VERSION}/node-v${NODE_ABI}-${PLATFORM}-${ARCH}.tar.gz`;
+octokit.repos
+  .getLatestRelease({
+    owner: PREBUILT_REPO_OWNER,
+    repo: PREBUILT_REPO_NAME
+  })
+  .then(({ data: { tag_name: VERSION }, status, headers }) => {
+    // handle data
+    const TARBALL_URL = `${PREBUILT_REPO_URL}/releases/download/${VERSION}/node-v${NODE_ABI}-${PLATFORM}-${ARCH}.tar.gz`;
     https.get(TARBALL_URL, response => {
       if (
         response.statusCode > 300 &&
@@ -84,4 +73,15 @@ fetch(PREBUILT_REPO_DATA_URL, {
         updateBinary(response);
       }
     });
-  });
+  })
+  .catch(e => console.error(e));
+
+// extract then update downloaded src
+function updateBinary(response) {
+  response.pipe(
+    extractTarGz({
+      cwd: CWD,
+      filter: (path, stat) => (path === "bin/index.node" ? true : false)
+    })
+  );
+}
